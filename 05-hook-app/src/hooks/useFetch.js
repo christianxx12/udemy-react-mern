daba72005ed90fbd804d434637d51d70edf6562c
Christian Rafael Guado Geronimo <crguado@gmail.com>
2024-08-12 20:35:19 -0500

    Incorporar caché

--- 05-hook-app/src/hooks/useFetch.js
+++ 05-hook-app/src/hooks/useFetch.js
@@ -1,12 +1,13 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react'
+
+const localCache = {}
 
 export function useFetch(url) {
-
   const [state, setState] = useState({
     data: null,
     isLoading: true,
     hasError: false,
-    error: null
+    error: null,
   })
 
   useEffect(() => {
@@ -18,13 +19,30 @@ export function useFetch(url) {
       data: null,
       isLoading: true,
       hasError: false,
-      error: null
+      error: null,
     })
   }
 
   async function getFetch() {
+    // si el localCache tiene un valor (es diferente de undefined)
+    if (localCache[url]) {
+      console.log('Usando caché')
+      setState({
+        data: localCache[url],
+        isLoading: false,
+        hasError: false,
+        error: null,
+      })
+      // al poner el return ya no ejecuta el setLoadingState y tampoco dispara la petición http ni el Sleep
+      return
+    }
+
     setLoadingState()
+
     const response = await fetch(url)
+
+    // Sleep
+    await new Promise((resolve) => setTimeout(resolve, 1500))
 
     if (!response.ok) {
       setState({
@@ -33,8 +51,8 @@ export function useFetch(url) {
         hasError: true,
         error: {
           code: response.status,
-          message: response.statusText
-        }
+          message: response.statusText,
+        },
       })
       return
     }
@@ -44,15 +62,16 @@ export function useFetch(url) {
       data: data,
       isLoading: false,
       hasError: false,
-      error: null
+      error: null,
     })
-    console.log(data)
 
+    //Manejo del caché
+    localCache[url] = data
   }
 
   return {
     data: state.data,
     isLoading: state.isLoading,
-    hasError: state.hasError
+    hasError: state.hasError,
   }
 }
