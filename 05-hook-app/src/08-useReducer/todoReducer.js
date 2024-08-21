41d39e040cbbddbba105b23902dc513aeb998a57
Christian Rafael Guado Geronimo <crguado@gmail.com>
2024-08-20 22:53:12 -0500

    useReducer - Todo List

---
+++ 05-hook-app/src/08-useReducer/todoReducer.js
@@ -0,0 +1,9 @@
+export function todoReducer(initialState = [], action) {
+  switch (action.type) {
+    case 'ABC':
+      throw new Error('Action.type = ABC no esta implementada')
+
+    default:
+      return initialState
+  }
+}
