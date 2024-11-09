1a02ef1eda45bd8229b0df4f0b9a6f86930b77ca
Christian Rafael <crguado@gmail.com>
2024-11-09 00:18:39 -0500

    useMemo

--- 05-hook-app/src/06-memos/MemoHook.jsx
+++ 05-hook-app/src/06-memos/MemoHook.jsx
@@ -1,46 +1,39 @@
-import { useMemo, useState } from 'react';
-import { useCounter } from '../hooks';
+import { useMemo, useState } from "react";
+import { useCounter } from "../hooks";
 
+function heavyStuff(iterationNumber = 100) {
+  for (let i = 0; i < iterationNumber; i++) {
+    console.log("Ahí vamos...");
+  }
 
-const heavyStuff = ( iterationNumber = 100 ) => {
-    for( let i = 0; i < iterationNumber; i++ ) {
-        console.log('Ahí vamos...');
-    }
-
-    return `${ iterationNumber } iteraciones realizadas`;
+  return `${iterationNumber} iteraciones realizadas`;
 }
-
-
 
 export const MemoHook = () => {
+  const { counter, increment } = useCounter(1000);
+  const [show, setShow] = useState(true);
 
-    const { counter, increment } = useCounter( 4000 );
-    const [ show, setShow ] = useState(true);
+  const memorizedValue = useMemo(() => heavyStuff(counter), [counter]);
 
-    const memorizedValue = useMemo( () => heavyStuff(counter), [counter])
+  return (
+    <>
+      <h1>
+        Counter: <small>{counter}</small>{" "}
+      </h1>
+      <hr />
 
-    return (
-        <>
-            <h1>Counter: <small>{ counter }</small>  </h1>
-            <hr />
+      <h4>{memorizedValue}</h4>
 
-            <h4>{ memorizedValue }</h4>
+      <button className="btn btn-primary" onClick={() => increment()}>
+        +1
+      </button>
 
-
-            <button
-                className="btn btn-primary"
-                onClick={ () => increment() }
-            >
-                +1
-            </button>
-
-            <button
-                className="btn btn-outline-primary"
-                onClick={ () => setShow( !show )  }
-            >
-                Show/Hide { JSON.stringify(show) } 
-            </button>
-        
-        </>
-    )
-}
+      <button
+        className="btn btn-outline-primary"
+        onClick={() => setShow(!show)}
+      >
+        Show/Hide {JSON.stringify(show)}
+      </button>
+    </>
+  );
+};
